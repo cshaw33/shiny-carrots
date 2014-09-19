ae3acdd608c3cd02a9f84448003195e87da9cb18
SpencerMalone <malone.spencer@gmail.com>
2014-09-19 18:04:43 -0400

    Update Constants.java
    Added SMS Constants

--- src/edu/gatech/fido/Constants.java
+++ src/edu/gatech/fido/Constants.java
@@ -4,6 +4,28 @@ package edu.gatech.fido;
  * Created by automation on 9/15/14.
  */
 public class Constants {
-    public static final int MAXINDOORHEIGHT = 1500;
+    public class DRONE {
+        public static final int MAXINDOORHEIGHT = 1500;
+    }
+
+    // SMS Constants
+    public class SMS {
+        public static final String STARTCOMMAND = "Drone ";
+        public static final String GOTOGPS = "goto ";
+        public static final String TAKEOFF = "takeoff";
+        public static final String LAND = "land";
+        public static final String GOFORWARD = "go forward ";
+        public static final String GOBACK = "go back ";
+        public static final String GOLEFT = "go left ";
+        public static final String GORIGHT = "go right ";
+        public static final String GOUP = "go up ";
+        public static final String GODOWN = "go down ";
+        public static final String TURNLEFT = "turn left ";
+        public static final String TURNRIGHT = "turn right ";
+        public static final String EMERGENCY = "emergency";
+        public static final String RESET = "reset";
+        public static final String PREFLIGHT = "preflight";
+        public static final String TRACKROTATION = "track rotation";
+    }
 
 }
