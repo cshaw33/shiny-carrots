8d5030d8be2358542aea01defbeb14389bb24a5a
SpencerMalone <malone.spencer@gmail.com>
2014-11-29 12:49:36 -0500

    Update DroneManager.java
    Updated to implement all possible commands, and also moved to using integers for the navigational commands

--- src/edu/gatech/fido/DroneManager.java
+++ src/edu/gatech/fido/DroneManager.java
@@ -7,7 +7,7 @@ import de.yadrone.base.ARDrone;
  */
 public class DroneManager {
     public static final int MAXINDOORHEIGHT = 1500;
-
+    public static boolean isLanded = true;
 
 
     private static ARDrone drone;
@@ -21,58 +21,78 @@ public class DroneManager {
 
     // This is used when we are on a flat surface to let the drone know that we are on a flat surface. Confusing na
     public static void preflightChecklist(){
-        drone.reset();
+       // drone.getCommandManager().emergency();
+     //   drone.reset();
         drone.getCommandManager().flatTrim();
         drone.setMaxAltitude(MAXINDOORHEIGHT);
         drone.start();
+
     }
 
     public static void goTo(String s) {
     }
 
     public static void takeOff() {
-                preflightChecklist();
-
-                drone.getCommandManager().takeOff();
+        drone.getCommandManager().takeOff();
+        drone.takeOff(); isLanded = false;
+        System.out.println("Took off!");
     }
 
     public static void land() {
-                drone.getCommandManager().landing();
-
+        drone.getCommandManager().landing(); isLanded = true;
     }
 
-    public static void forward(String s) {
+    public static void forward(int s) {
+        drone.getCommandManager().forward(s);
     }
 
-    public static void back(String s) {
+    public static void back(int s) {drone.getCommandManager().backward(s);
     }
 
-    public static void left(String s) {
+    public static void left(int s) {drone.getCommandManager().goLeft(s);
     }
 
-    public static void right(String s) {
+    public static void right(int s) {drone.getCommandManager().goRight(s);
     }
 
-    public static void up(String s) {
+    public static void up(int s) {drone.getCommandManager().up(s);
     }
 
-    public static void down(String s) {
+    public static void down(int s) {drone.getCommandManager().down(s);
     }
 
-    public static void turnCounterClockwise(String s) {
+    public static void turnCounterClockwise(int s) {
+        drone.getCommandManager().spinLeft(s);
+        drone.spinLeft();
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
-    public static void turnClockwise(String s) {
+    public static void turnClockwise(int s) {
+        drone.getCommandManager().spinLeft(s);
+        drone.spinRight();
+
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
     }
 
     public static void emergency() {
+        drone.getCommandManager().emergency();
     }
 
     public static void reset() {
+        drone.reset();
+        preflightChecklist();
     }
 
-    public static void trackRotation() {
-    }
+
 
 
 }
