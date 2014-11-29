91fee36ed146007d8f50cca83914df82092a8573
SpencerMalone <malone.spencer@gmail.com>
2014-11-29 12:46:37 -0500

    Update GPSManager.java
    Moved bearing calculations to the compass class, as the compass sensor has a higher tick rate than the GPS

--- src/edu/gatech/fido/GPSManager.java
+++ src/edu/gatech/fido/GPSManager.java
@@ -4,14 +4,15 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import edu.gatech.fido.Constants.GPS;
-
-
+/**
+ * Created by automation on 9/15/14.
+ */
 public class GPSManager
 {
 
-
     private static Location dogLocation;
     static double desiredBearing;
+    public static boolean gpsLoaded = false;
     public static Location getDroneLocation() {
         return droneLocation;
     }
@@ -23,25 +24,28 @@ public class GPSManager
     }
     public static void Initialize(LocationManager locationManager)
     {
+        System.out.println("GPS Initialization starting!");
+
 
         dogLocation = new Location("dogVest");
 // Define a listener that responds to location updates
         LocationListener locationListener = new LocationListener() {
 
             public void onLocationChanged(Location location) {
+
 // Called when a new location is found by the network location provider.
-                System.out.println("Hello!");
                 makeUseOfNewLocation(location);
             }
             public void onStatusChanged(String provider, int status, Bundle extras) {
-                System.out.println("asdf1");}
+                System.out.println("Status Change!");}
             public void onProviderEnabled(String provider) {
-                System.out.println("asdf2");}
+                System.out.println("Provider Enabled!");}
             public void onProviderDisabled(String provider) {
-                System.out.println("asdf3");}
+                System.out.println("Provider Disabled!");}
         };
 // Register the listener with the Location Manager to receive location updates
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+        System.out.println("GPS Initialized!");
 
     }
     public static void setDestination(String text)
@@ -52,45 +56,30 @@ public class GPSManager
     }
     public static void makeUseOfNewLocation(Location dLocation)
     {
+        System.out.println("New location!");
         droneLocation = dLocation;
         if (droneLocation.getAccuracy() < GPS.MIN_DISTANCE_PRECISION)
-        {
-            float distanceRemaining = (droneLocation.distanceTo(dogLocation));
-            if (distanceRemaining < GPS.MIN_DISTANCE_CORRECTION || distanceRemaining < 2 * droneLocation.getAccuracy())
+            if(true)
             {
-                DroneManager.land();
-                System.out.println("arrived at Destination");
-            } else {
-                if (droneLocation.hasBearing())
-                {
-                    Double curDegree = Compass.getCurDegree();
-             //       double currentBearing = droneLocation.getBearing();
-                    desiredBearing = droneLocation.bearingTo(dogLocation);
-                    double desiredDegree = (curDegree - desiredBearing);
-
-                    if(Math.abs(desiredDegree) > 180){
-                        desiredDegree += desiredDegree > 0 ? -360 : 360;
-                    }
-
-                    if(desiredDegree < 0){
-                        System.out.println("Turn right!");
-                        DroneManager.turnClockwise(String.valueOf(Math.min(Math.abs(desiredDegree),100)));
-                    }
-                    else if(desiredDegree > 0)
-                    {
-                        System.out.println("Turn left!");
-                        DroneManager.turnCounterClockwise(String.valueOf(Math.min(Math.abs(desiredDegree),100)));
-                    }
-               //     double bearingDifference = currentBearing - desiredBearing;
-               //     if (Math.abs(bearingDifference) > GPS.MIN_BEARING_CORRECTION)
-                 //   {
-                  //  }
+                if(DroneManager.isLanded){
+                    DroneManager.takeOff();
+                    DroneManager.isLanded = false;
                 }
+
+                float distanceRemaining = (droneLocation.distanceTo(dogLocation));
+                System.out.println(distanceRemaining);
+                if (distanceRemaining < GPS.MIN_DISTANCE_CORRECTION )
+                {
+                    DroneManager.land();
+                    System.out.println("arrived at Destination");
+                } else {
+
+                    gpsLoaded = true;
+
+                }
+            } else {
+                System.out.println("Location not precise enough, waiting for better location");
             }
-        } else {
-            System.out.println("Location not precise enough, waiting for better location");
-        }
-        System.out.println("found location");
         System.out.println(droneLocation.toString());
     }
 }
